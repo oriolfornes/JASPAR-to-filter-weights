e385ea2791b352c4f68ee3cf4349477ff9693dbd
Oriol Fornes <oriol.fornes@gmail.com>
2021-05-07 08:32:40 -0700

    added filter weights

--- jaspar2nr-filter-weights.py
+++ jaspar2nr-filter-weights.py
@@ -27,11 +27,11 @@ def main(**params):
 
     # Initialize
     base_dir = os.path.dirname(os.path.realpath(__file__))
-    motifs_dir = os.path.join(base_dir, "motifs")
+    data_dir = os.path.join(base_dir, "data")
 
     # Parse binding modes
     binding_modes = {}
-    binding_modes_file = os.path.join(motifs_dir, "leaf_to_cluster.tab")
+    binding_modes_file = os.path.join(data_dir, "leaf_to_cluster.tab")
     with open(binding_modes_file) as handle:
         for line in handle:
             matrix_id, cluster = line.strip("\n").split("\t")
@@ -43,7 +43,7 @@ def main(**params):
     # Parse profiles
     profiles = {}
     ixs = {"A":0, "C":1, "G":2, "T":3}
-    jaspar_file = os.path.join(motifs_dir,
+    jaspar_file = os.path.join(data_dir,
         "JASPAR2020_CORE_vertebrates_redundant_pfms_jaspar.txt")
     with open(jaspar_file) as handle:
         for m in motifs.parse(handle, "jaspar"):
@@ -60,21 +60,21 @@ def main(**params):
         m = profiles[binding_modes[bm][0]][0]
         filters.setdefault(m.matrix_id, [m.name, bm])
         pwm = [list(i) for i in m.pwm.values()]
-        pwm = _resize_PWM(list(map(list, zip(*pwm))), params["filter_size"])
+        pwm = _PWM_to_filter_weights(list(map(list, zip(*pwm))), params["filter_size"])
         filters[m.matrix_id].append(pwm)
         filters[m.matrix_id].append(np.flip(pwm))
 
     # Save
-    with open(params["out_file"], 'wb') as handle:
+    with open(params["out_file"], "wb") as handle:
         pickle.dump(filters, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-def _resize_PWM(pwm, filter_size=19):
+def _PWM_to_filter_weights(pwm, filter_size=19):
 
     # Initialize
     lpop = 0
     rpop = 0
 
-    pwm = [[0., 0., 0., 0.]]*filter_size + pwm + [[0., 0., 0., 0.]]*filter_size
+    pwm = [[.25,.25,.25,.25]]*filter_size+pwm+[[.25,.25,.25,.25]]*filter_size
 
     while len(pwm) > filter_size:
         if max(pwm[0]) < max(pwm[-1]):
@@ -91,7 +91,7 @@ def _resize_PWM(pwm, filter_size=19):
                 pwm.pop(0)
                 lpop += 1
 
-    return(np.array(pwm))
+    return(np.array(pwm) - .25)
 
 if __name__ == "__main__":
     main()
---
+++ jaspar2tf-filter-weights.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+from Bio import motifs
+import click
+import numpy as np
+import os
+import pickle
+
+CONTEXT_SETTINGS = {
+    "help_option_names": ["-h", "--help"],
+}
+
+@click.command(no_args_is_help=True, context_settings=CONTEXT_SETTINGS)
+@click.argument(
+    "out_file",
+    type=click.Path(resolve_path=True),
+)
+@click.option(
+    "-f", "--filter-size",
+    help="Filter size.",
+    type=int,
+    default=19,
+    show_default=True,
+)
+
+def main(**params):
+
+    # Initialize
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    data_dir = os.path.join(base_dir, "data")
+
+    # Parse profiles
+    filters = {}
+    jaspar_file = os.path.join(data_dir,
+        "JASPAR2020_CORE_vertebrates_non-redundant_pfms_jaspar.txt")
+    with open(jaspar_file) as handle:
+        for m in motifs.parse(handle, "jaspar"):
+            m.pseudocounts = motifs.jaspar.calculate_pseudocounts(m)
+            tfs = m.name.upper().split("(")[0].split("::")
+            pwm = [list(i) for i in m.pwm.values()]
+            pwm = _PWM_to_filter_weights(list(map(list, zip(*pwm))), params["filter_size"])
+            for tf in tfs:
+                filters.setdefault(tf, [])
+                filters[tf].append([m.matrix_id, pwm, np.flip(pwm)])
+
+    # Save
+    with open(params["out_file"], "wb") as handle:
+        pickle.dump(filters, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def _PWM_to_filter_weights(pwm, filter_size=19):
+
+    # Initialize
+    lpop = 0
+    rpop = 0
+
+    pwm = [[.25,.25,.25,.25]]*filter_size+pwm+[[.25,.25,.25,.25]]*filter_size
+
+    while len(pwm) > filter_size:
+        if max(pwm[0]) < max(pwm[-1]):
+            pwm.pop(0)
+            lpop += 1
+        elif max(pwm[-1]) < max(pwm[0]):
+            pwm.pop(-1)
+            rpop += 1
+        else:
+            if lpop > rpop:
+                pwm.pop(-1)
+                rpop += 1
+            else:
+                pwm.pop(0)
+                lpop += 1
+
+    return(np.array(pwm) - .25)
+
+if __name__ == "__main__":
+    main()
