fcf64ef86bcd7e9c6cb0048269b5031d55ac1f5d
oriolfornes <oriol.fornes@gmail.com>
2021-11-04 11:28:38 -0700

    added binding modes pickle

--- jaspar2filter-weights-per-binding-mode.py
+++ jaspar2filter-weights-per-binding-mode.py
@@ -22,8 +22,15 @@ CONTEXT_SETTINGS = {
     default=19,
     show_default=True,
 )
+@click.option(
+    "-w", "--weights",
+    help="Type of weights.",
+    type=click.Choice(["PFM", "PWM"], case_sensitive=False),
+    default="PFM",
+    show_default=True,
+)
 
-def main(**params):
+def main(**args):
 
     # Initialize
     base_dir = os.path.dirname(os.path.realpath(__file__))
@@ -55,21 +62,34 @@ def main(**params):
         for matrix_id in binding_modes[bm]:
             m = profiles[matrix_id]
             filters.setdefault(bm, [])
-            pwm = [list(i) for i in m.pwm.values()]
-            pwm = _PWM_to_filter_weights(list(map(list, zip(*pwm))), params["filter_size"])
-            filters[bm].append([matrix_id, pwm, np.flip(pwm)])
+            pwm = _PWM_to_filter_weights(m, args["weights"],
+                args["filter_size"])
+            filters[bm].append([matrix_id, pwm, pwm[::-1,::-1]])
 
     # Save
-    with open(params["out_file"], "wb") as handle:
+    with open(args["out_file"], "wb") as handle:
         pickle.dump(filters, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-def _PWM_to_filter_weights(pwm, filter_size=19):
+def _PWM_to_filter_weights(motif, weights="PFM", filter_size=19):
 
     # Initialize
     lpop = 0
     rpop = 0
 
-    pwm = [[.25,.25,.25,.25]]*filter_size+pwm+[[.25,.25,.25,.25]]*filter_size
+    # If PFM...
+    if weights == "PFM":
+        pwm = np.array([list(i) for i in motif.pwm.values()]) - .25
+    # ... Else...
+    else:
+        motif.pseudocounts = motifs.jaspar.calculate_pseudocounts(motif)
+        pwm = np.array([list(i) for i in motif.pssm.values()])
+
+    # Extend the PWM with zeros to the left and right
+    pwm = np.concatenate((np.zeros((4, filter_size)), pwm,
+        np.zeros((4, filter_size))), axis=1)
+
+    # Transpose PWM
+    pwm = pwm.T.tolist()
 
     while len(pwm) > filter_size:
         if max(pwm[0]) < max(pwm[-1]):
@@ -86,7 +106,7 @@ def _PWM_to_filter_weights(pwm, filter_size=19):
                 pwm.pop(0)
                 lpop += 1
 
-    return(np.array(pwm) - .25)
+    return(np.array(pwm))
 
 if __name__ == "__main__":
     main()
--- jaspar2nr-filter-weights.py
+++ jaspar2nr-filter-weights.py
@@ -34,8 +34,15 @@ CONTEXT_SETTINGS = {
     default=100,
     show_default=True,
 )
+@click.option(
+    "-w", "--weights",
+    help="Type of weights.",
+    type=click.Choice(["PFM", "PWM"], case_sensitive=False),
+    default="PFM",
+    show_default=True,
+)
 
-def main(**params):
+def main(**args):
 
     # Initialize
     base_dir = os.path.dirname(os.path.realpath(__file__))
@@ -63,65 +70,73 @@ def main(**params):
 
     # For each binding mode...
     filters = {}
+    filters_pfms = {}
     for bm in sorted(binding_modes):
         for matrix_id in binding_modes[bm]:
             m = profiles[matrix_id]
             tfs = m.name.upper().split("(")[0].split("::")
-            pwm = [list(i) for i in m.pwm.values()]
-            pwm = _PWM_to_filter_weights(list(map(list, zip(*pwm))), params["filter_size"])
+            pwm = _PWM_to_filter_weights(m, args["weights"],
+                args["filter_size"])
             filters.setdefault("%s;fwd;%s" % (matrix_id, "::".join(tfs)), pwm)
-            filters.setdefault("%s;rev;%s" % (matrix_id, "::".join(tfs)), np.flip(pwm))
-            break
+            filters.setdefault("%s;rev;%s" % (matrix_id, "::".join(tfs)),
+                pwm[::-1,::-1])
+            pfm = _PWM_to_filter_weights(m, filter_size=args["filter_size"])
+            filters_pfms.setdefault("%s;fwd;%s" % (matrix_id, "::".join(tfs)),
+                pfm)
+            filters_pfms.setdefault("%s;rev;%s" % (matrix_id, "::".join(tfs)),
+                pfm[::-1,::-1])
 
     # MEME
     meme_file = os.path.join(base_dir,
-        "JASPAR2020_CORE_vertebrates_non-redundant_pfms_filter-size=%s.meme" % str(params["filter_size"]))
+        "JASPAR2020_CORE_vertebrates_nr_f=%s.meme" % str(args["filter_size"]))
     if not os.path.exists(meme_file):
 
-        lines = ""
-        lines += "MEME version 4\n\n"
-        lines += "ALPHABET= ACGT\n\n"
-        lines += "strands: + -\n\n"
-        lines += "Background letter frequencies (from uniform background):\n"
-        lines += "A 0.25000 C 0.25000 G 0.25000 T 0.25000\n\n"
+        s = ""
+        s += "MEME version 4\n\n"
+        s += "ALPHABET= ACGT\n\n"
+        s += "strands: + -\n\n"
+        s += "Background letter frequencies (from uniform background):\n"
+        s += "A 0.25000 C 0.25000 G 0.25000 T 0.25000\n\n"
 
-        for name in sorted(filters):
-            m = "\n".join(["\t".join(list(map(str, i+.25))) for i in filters[name]])
+        for name in sorted(filters_pfms):
+            m = "\n".join(["\t".join(list(map(str, i+.25))) \
+                for i in filters_pfms[name]])
             handle = io.StringIO(m)
             m = motifs.read(handle, "pfm-four-columns")
             consensus_seq = m.consensus
             w = len(consensus_seq)
-            lines += "MOTIF %s %s\n" % (name, consensus_seq)
-            lines += "letter-probability matrix: alength= 4 w= %s nsites= 20 E= 0\n" % w
+            s += "MOTIF %s %s\n" % (name, consensus_seq)
+            s += "letter-probability matrix: alength= 4 w= %s nsites= 20 E= 0\n" % w
             pwm = [m.pwm[nt] for nt in "ACGT"]
             for row in np.transpose(np.array(pwm)):
-                lines += " ".join([str(round(r, 8)).rjust(11) for r in row]) + "\n"
-            lines += "\n"
+                s += " ".join([str(round(r, 8)).rjust(11) for r in row]) + "\n"
+            s += "\n"
 
         with open(meme_file, "w") as handle:
-            handle.write(lines)
+            handle.write(s)
 
     # TOMTOM
     tomtom_file = os.path.join(base_dir,
-        "JASPAR2020_CORE_vertebrates_non-redundant_pfms_filter-size=%s.tsv" % str(params["filter_size"]))
+        "JASPAR2020_CORE_vertebrates_nr_f=%s.tsv" % str(args["filter_size"]))
     if not os.path.exists(tomtom_file):
 
-        cmd = "tomtom %s %s -thresh 5000 -evalue -norc --text > %s" % (meme_file, meme_file,
-            tomtom_file)
+        cmd = "tomtom %s %s -thresh 5000 -evalue -norc --text > %s" % (
+            meme_file, meme_file, tomtom_file)
         _ = sp.run([cmd], shell=True)
 
     # Non-redundant filters
     ixs = {}
     tomtom = []
     nr_filters = {}
-    df = pd.read_csv(tomtom_file, sep="\t", header=0, usecols=[0, 1, 5], comment="#")
+    df = pd.read_csv(tomtom_file, sep="\t", header=0, usecols=[0, 1, 5],
+        comment="#")
     df.sort_values(by=["Query_ID", "Target_ID"], inplace=True)
     matrix_ids = list(df["Query_ID"].unique())
     for ix, matrix_id in enumerate(matrix_ids):
         ixs.setdefault(matrix_id, ix)
         tomtom.append(df[df["Query_ID"] == matrix_id]["q-value"].to_list())
     random.shuffle(matrix_ids)
-    while len(nr_filters) < params["n_filters"] and len(matrix_ids) > 0:
+    while len(nr_filters) < args["n_filters"] and len(matrix_ids) > 0:
         matrix_id = matrix_ids.pop(0)
         is_nr = True
         for nr_matrix_id in nr_filters:
@@ -132,16 +147,29 @@ def main(**params):
             nr_filters.setdefault(matrix_id, filters[matrix_id])
 
     # Save
-    with open(params["out_file"], "wb") as handle:
+    with open(args["out_file"], "wb") as handle:
         pickle.dump(nr_filters, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-def _PWM_to_filter_weights(pwm, filter_size=19):
+def _PWM_to_filter_weights(motif, weights="PFM", filter_size=19):
 
     # Initialize
     lpop = 0
     rpop = 0
 
-    pwm = [[.25,.25,.25,.25]]*filter_size+pwm+[[.25,.25,.25,.25]]*filter_size
+    # If PFM...
+    if weights == "PFM":
+        pwm = np.array([list(i) for i in motif.pwm.values()]) - .25
+    # ... Else...
+    else:
+        motif.pseudocounts = motifs.jaspar.calculate_pseudocounts(motif)
+        pwm = np.array([list(i) for i in motif.pssm.values()])
+
+    # Extend the PWM with zeros to the left and right
+    pwm = np.concatenate((np.zeros((4, filter_size)), pwm,
+        np.zeros((4, filter_size))), axis=1)
+
+    # Transpose PWM
+    pwm = pwm.T.tolist()
 
     while len(pwm) > filter_size:
         if max(pwm[0]) < max(pwm[-1]):
@@ -158,7 +186,7 @@ def _PWM_to_filter_weights(pwm, filter_size=19):
                 pwm.pop(0)
                 lpop += 1
 
-    return(np.array(pwm) - .25)
+    return(np.array(pwm))
 
 if __name__ == "__main__":
     main()
